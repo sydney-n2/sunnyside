78c2100d59d020117886c3a292299218d8673944
sydney-n2 <76408188+sydney-n2@users.noreply.github.com>
2023-05-19 01:23:34 -0700

    login screen done
    added elements and a bit of styling, still need to size them

--- App.js
+++ App.js
@@ -2,21 +2,25 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import * as React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView, TextInput, SafeAreaView, TouchableWithoutFeedback, TouchableHighlight, Button, Pressable } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, TextInput, SafeAreaView, TouchableWithoutFeedback, TouchableHighlight, Pressable} from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 function LoginScreen({navigation}) {
   return (
     <SafeAreaView style={styles.container}>
-    <Text>Sunnyside</Text>
     <Image
       source={require('./assets/sunnyside-logo.png')} 
       //style={{width: 300, height: 200}}
         //network images require dimensions bc it doesn't know, for assets use require and it knows the metadata
     />
-
-    <Button title='Login' onPress={()=>navigation.navigate('Home')}></Button>
+    <TextInput style={styles.input} placeholder="Username or Email"/>
+    <TextInput style={styles.input} placeholder="Password" secureTextEntry={true}/>
+    {/* <Button style={styles.button} title='Login' onPress={()=>navigation.navigate('Home')}></Button>
+    buttons have very limited styling, do not use  */}
+    <Pressable style={styles.button} onPress={()=>navigation.navigate('Home')}>
+      <Text>Login</Text> 
+    </Pressable>
     <Text style={styles.text}>Don't have an account? <Text style={{textDecorationLine: 'underline'}} onPress={()=>navigation.navigate('Register')}>Register here!</Text>
     </Text>
   </SafeAreaView>
@@ -26,7 +30,8 @@ function LoginScreen({navigation}) {
 function RegisterScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text> Email</Text>
+          <Image source={require('./assets/sunnyside-logo.png')}    />
+
     </SafeAreaView>
   )
 }
@@ -42,11 +47,11 @@ function HomeScreen() {
 export default function App() {
   return (
     <NavigationContainer> 
-    <Stack.Navigator>
+    <Stack.Navigator> 
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Register" component={RegisterScreen}/>
       <Stack.Screen name="Home" component={HomeScreen}/> 
-
+{/* need to make the navigator bar thing disappear later */}
     </Stack.Navigator>
     </NavigationContainer> 
   );
@@ -58,6 +63,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#FAECA6',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 10
   },
   
   text: {
@@ -66,5 +72,18 @@ const styles = StyleSheet.create({
 
   image: {
     marginBottom: 40
+  },
+
+  button: {
+    backgroundColor: '#F9C980',
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20
+  },
+
+  input: {
+    backgroundColor: '#FFFFFF',
+    padding: 5
   }
 });
